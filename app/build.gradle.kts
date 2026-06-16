ea2daec1d1b079b885a6699f12fa42a359294bf3
Piotr Jakubowski <piotr.jakubowski@gmail.com>
2026-06-16 14:07:04 +0200

    build: bump versionCode to 4 and versionName to 1.2.0 for production release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.indiana.focustimer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
