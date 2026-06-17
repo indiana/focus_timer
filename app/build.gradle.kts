01638cadf111e16f85f20c8cb05906908579b922
indiana <piotr.jakubowski@gmail.com>
2026-06-17 13:30:37 +0200

    fix: route alarm sounds to system USAGE_ALARM stream and bump version to 1.2.1 (#6)
    * fix: route alarm sounds to system USAGE_ALARM stream and bump version to 1.2.1 (build 5)
    
    * ci: run release build steps only on master branch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.indiana.focustimer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
